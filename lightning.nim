02ac38025c14730d3eb581580c3edea5c527e305
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-11-03 22:54:02 +0100

    changes before rc presentation

--- lightning.nim
+++ lightning.nim
@@ -1,15 +1,28 @@
 import nimib, nimislides
 
-nbInit(theme = revealTheme)
-setSlidesTheme(Solarized)
+template slideIframe*(url: string) =
+  nbRawHtml: "<section data-background-iframe=\"" & url & "\" data-background-interactive></section>"
+
+template slideIframeFromNblog*(filename: string) =
+  slideIframe("https://pietroppeter.github.io/nblog/drafts/" & filename & ".html")
+
+# small text
+template addNbTextSmall* =
+  nb.partials["nbTextSmall"] = "<small>" & nb.partials["nbText"] & "</small>"
+  nb.renderPlans["nbTextSmall"] = nb.renderPlans["nbText"]
+
+template nbTextSmall*(text: string) =
+  nbText: text
+  nb.blk.command = "nbTextSmall"
 
 template slideTitle =
   slide:
     bigText: "Nim 👑 for Pythonistas 🐍"
     nbText: "[github.com/pietroppeter/nim-for-pythonistas](https://github.com/pietroppeter/nim-for-pythonistas)"
+    nbText: "_WIP for PyCon🇸🇪 Nov 9-10_"
 
 template reference(text: string) =
-  nbText: text
+  nbTextSmall: text
 
 template slideWhatIsNim =
   slide:
@@ -58,6 +71,9 @@ template slideSyntax =
             for i in g.low .. g.high:
               for j in g[i].low .. g[i].high:
                 g[i][j] = if g[i][j] == X: O else: X
+        nbTextSmall: "- syntax fits well with **metaprogramming**\n" &
+        "- UFCS (Uniform Function Call Syntax)"
+        nbTextSmall: "[Zen of Nim (2021)](https://nim-lang.org/blog/2021/11/15/zen-of-nim.html)"
 
       column:
         nbText: "⠀" # https://www.compart.com/en/unicode/U+2800
@@ -65,30 +81,43 @@ template slideSyntax =
         #nbText: "Indentation based syntax that fits Nim's macro system"
         nbCode:
           var g = initGrid()
-          show g
+          show g # command
         nbCode:          
-          g.update
+          g.update # method
           g.show
         nbCode:
-          update(g)
-          show g
-    reference "[Zen of Nim (2021)](https://nim-lang.org/blog/2021/11/15/zen-of-nim.html)"
+          update(g) # function
+          show(g)
+    
 
 template slidePerformant =
   slide:
-    nbText: "### Nim is Performant"
+    nbText: "### Performant: compiles to C"
+    nbText: "_todo: matrix multiplications_"
+    #nbText: "_came for the performance, stayed for the ergonomics_"
 
 template slidePragmatic =
   slide:
-    nbText: "### Nim is Pragmatic"
+    nbText: "### Interop with Python"
+    reference "[nimporter (based on nimpy)](https://github.com/Pebaz/nimporter)"
+    #slide:
+    #  slideIframe "https://github.com/Pebaz/nimporter"
 
 template slidePortable =
   slide:
-    nbText: "### Nim is Portable"
+    nbText: "### Compiles to Javascript"
+    nbText: "nim produces small native executables and "
+    reference "[🌱✨ plant app](https://pietroppeter.github.io/nblog/drafts/plant_app.html?utm_source=github-pietroppeter)"    
+    #slide:
+    #  nbText: ""
+    #  slideIframeFromNblog("plant_app")
 
-template slideProductive =
+template slideEverything =
   slide:
-    nbText: "### Nim is Productive"
+    nbText: "### For everything"
+    unorderedList:
+      listItem: nbText"gamedev: nico, godot bindings (Turing Complete)"
+      listItem: nbText"langdev: arturo, min"
 
 template slideSource =
   slide:
@@ -97,13 +126,20 @@ template slideSource =
     reference "[nimislides by Hugo]()"
 
 when isMainModule:
+  nbInit(theme = revealTheme)
+  setSlidesTheme(Solarized)
+  addNbTextSmall
+
   when false:
-    slideTitle
-    slideWhatIsNim 
-    slideSyntax
+    discard
+  slideTitle
+  slideWhatIsNim 
+  slideAlternativeToRust
+  slideSyntax
   slidePerformant
   slidePragmatic
   slidePortable
-  slideProductive
+  #slideEverything
+  #slideProductive
   slideSource
   nbSave
