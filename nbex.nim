b617be8a01eaecf857f2283c9c9d961abfaf666b
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2024-05-03 11:09:15 +0200

    nbTextColor

--- nbex.nim
+++ nbex.nim
@@ -1,6 +1,17 @@
-import nimislides
+import nimib, nimislides
 
 template mySlide*(ident: untyped, body: untyped) =
   template ident* =
     slide:
       body
+
+template nbTextColor*(col: string, text: string) =
+  nbRawHtml("<span style=\"color:" & col & ";\">")
+  nbText text
+  nbRawHtml("""</span">""")
+
+when isMainModule:
+  nbInit(theme=revealTheme)
+  slide:
+    nbTextColor("red"): "hi"
+  nbSave
