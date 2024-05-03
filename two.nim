10746f3425940fb89fd2b9ff70fc19944f8af8da
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2024-05-04 00:27:53 +0200

    ideas on two good!

--- two.nim
+++ two.nim
@@ -1,5 +1,16 @@
 import nimib, nimislides, nbex
 
+
+template all* =
+  # const fizzbuzz (compile time evaluation) - when?
+  # let and var (mutability)
+  # func (side effects)
+  # what on type system? object vs ref object? enums? distinct?
+  # or maybe just an enumeration of type system in two slides (primitive and structured)
+  # maybe just what is needed to explain jsony
+  discard
+
+
 when isMainModule:
   nbInit(revealTheme)
   nbSave
